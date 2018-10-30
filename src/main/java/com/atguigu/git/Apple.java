583685f816cb51213eb0962f54e52c412fc3cd39
tom_glb <goodMorning_glb@atguigu.com>
2018-10-30 15:15:34 +0800

    hhhhhhh

--- src/main/java/com/atguigu/git/Apple.java
+++ src/main/java/com/atguigu/git/Apple.java
@@ -1,5 +1,10 @@
 package com.atguigu.git;
 
+import org.omg.Messaging.SyncScopeHelper;
+
 public class Apple {
+	public static void main(String[] args) {
+		System.out.println("hot fix...");
+	}
 
 }
